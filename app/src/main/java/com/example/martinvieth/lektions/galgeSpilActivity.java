fdc6c96500fc6a5ba77f23a60f374e1a0b161bca
Kenneth Frandsen <Frandsen86@gmail.com>
2015-11-09 11:53:21 +0100

    test

--- app/src/main/java/com/example/martinvieth/lektions/galgeSpilActivity.java
+++ app/src/main/java/com/example/martinvieth/lektions/galgeSpilActivity.java
@@ -40,7 +40,7 @@ public class galgeSpilActivity extends Activity  implements OnClickListener {
         //text box for guess
         eTGuess = (EditText) findViewById(R.id.eTGuess);
 
-
+//
         //Text view for error or messages
         twBesked = (TextView) findViewById(R.id.twBesked);
         twHiddenWord = (TextView) findViewById(R.id.twHiddenWord);
