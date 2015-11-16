ac8f2f4ef277d13dab2cc7f1073504cc949b3e9a
Pede <p.asmussen.git@gmail.com>
2015-11-16 09:13:26 +0100

    Listview virker nu (y) (før var det et ScrollView, ups)

--- app/src/main/java/com/example/martinvieth/lektions/ordFraDR.java
+++ app/src/main/java/com/example/martinvieth/lektions/ordFraDR.java
@@ -5,16 +5,20 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 public class ordFraDR extends Activity implements View.OnClickListener{
 
     //Tilføjer textview, knap og arraylist til at putte ord fra hentOrdFraDr() ind i
-    TextView ordFraDr;
+    ListView ordFraDr;
+    TextView hentOrdStatus;
     Button btnOrd;
     ArrayList<String> ord = new ArrayList();
 
@@ -24,7 +28,9 @@ public class ordFraDR extends Activity implements View.OnClickListener{
         setContentView(R.layout.activity_ord_fra_dr);
 
 
-        ordFraDr = (TextView) findViewById(R.id.ordFraDr);
+        ordFraDr = (ListView) findViewById(R.id.ordFraDr);
+        hentOrdStatus = (TextView) findViewById(R.id.hentOrdStatus);
+        hentOrdStatus.setText("Henter ord...");
         btnOrd = (Button) findViewById(R.id.btnOrd);
         btnOrd.setOnClickListener(this);
 
@@ -36,16 +42,16 @@ public class ordFraDR extends Activity implements View.OnClickListener{
             protected Object doInBackground(Object... args0) {
                 try {
                     galgeSpilActivity.galgelogik.hentOrdFraDr();
-                    return "Følgende ord er hentet fra DR:" + "\n" ;
                 } catch (Exception e) {
                     e.printStackTrace();
-                    return "Ord blev ikke hentet fra DR!";
                 }
+                return "Success";
             }
+
 
             @Override
             protected void onPostExecute(Object resultat) {
-                ordFraDr.setText("\n" + resultat);
+                hentOrdStatus.setText("Ord hentet!");
             }
         };
         asyncTask.execute();
@@ -57,12 +63,12 @@ public class ordFraDR extends Activity implements View.OnClickListener{
         //Knap-funktionalitet, henter ordene via getMuligeOrd(), putter dem ind i arraylist "ord"
         if (v == btnOrd) {
             ord = galgeSpilActivity.galgelogik.getMuligeOrd();
-            for (String s : ord){
-                ordFraDr.append(s + "\n");
-            }
-            ordFraDr.setVerticalScrollBarEnabled(true);
+            ordFraDr.setAdapter(new ArrayAdapter(this,
+                    android.R.layout.simple_list_item_1, android.R.id.text1, galgeSpilActivity.galgelogik.getMuligeOrd()));
+
         }
     }
 }
+
 
 
