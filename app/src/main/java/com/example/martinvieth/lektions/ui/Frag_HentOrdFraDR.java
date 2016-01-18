b34746fc08d28b786ee38ab1cecc2fdd139a21ef
Pede <p.asmussen.git@gmail.com>
2016-01-18 14:38:41 +0100

    Hmm..

--- app/src/main/java/com/example/martinvieth/lektions/ui/Frag_HentOrdFraDR.java
+++ app/src/main/java/com/example/martinvieth/lektions/ui/Frag_HentOrdFraDR.java
@@ -9,72 +9,66 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.martinvieth.lektions.R;
+import com.example.martinvieth.lektions.logic.Galgelogik;
 
 import java.util.ArrayList;
 
 public class Frag_HentOrdFraDR extends Fragment implements View.OnClickListener{
 
     //Tilføjer textview, knap og arraylist til at putte ord fra hentOrdFraDr() ind i
-    ListView ordFraDr;
-    TextView hentOrdStatus;
+    EditText hentOrdUrl;
+    ListView ordFraUrl;
     Button btnOrd;
     ArrayList<String> ord = new ArrayList<>();
-    static String selectedWord;
+    Galgelogik gl;
+    String url;
 
     @Override
     public View onCreateView(LayoutInflater i, ViewGroup container, Bundle savedInstanceState) {
         View rod = i.inflate(R.layout.frag_hent_ord_fra_dr, container, false);
 
+        if (gl == null){
+            gl = new Galgelogik();
+        }
 
-        ordFraDr = (ListView) rod.findViewById(R.id.ordFraDr);
-        ordFraDr.setClickable(true);
-        hentOrdStatus = (TextView) rod.findViewById(R.id.hentOrdStatus);
-        hentOrdStatus.setTextColor(Color.rgb(248, 248, 255));
-        hentOrdStatus.setText("Henter ord...");
+        ordFraUrl = (ListView) rod.findViewById(R.id.ordFraDr);
+        ordFraUrl.setClickable(true);
+        hentOrdUrl = (EditText) rod.findViewById(R.id.hentOrdUrl);
+        hentOrdUrl.setTextColor(Color.rgb(248, 248, 255));
         btnOrd = (Button) rod.findViewById(R.id.btnOrd);
         btnOrd.setOnClickListener(this);
 
-
-        //AsyncTask, taget fra opgave-vejledningen på campusnet
-
-        AsyncTask asyncTask = new AsyncTask() {
-            @Override
-            protected Object doInBackground(Object... args0) {
-                Activity_Main main = (Activity_Main) getActivity();
-                if(main.isNetworkAvailable()) {
-                    try {
-                        Frag_Galgespil.getLogic().hentOrdFraDr();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                } else Toast.makeText(getActivity(), "Det ser ud til, at der er problemer med internetforbindelsen.",
-                        Toast.LENGTH_SHORT).show();
-                return null;
-            }
-
-
-            @Override
-            protected void onPostExecute(Object resultat) {
-                hentOrdStatus.setText("Ord hentet!");
-            }
-        };
-        asyncTask.execute();
         return rod;
     }
 
     @Override
     public void onClick(View v) {
 
-        //Knap-funktionalitet, henter ordene via getMuligeOrd(), putter dem ind i arraylist "ord"
         if (v == btnOrd) {
-            ord = Frag_Galgespil.getLogic().getMuligeOrd();
-            ordFraDr.setAdapter(new ArrayAdapter<>(getActivity(),
-                    android.R.layout.simple_list_item_1, android.R.id.text1, Frag_Galgespil.getLogic().getMuligeOrd()));
+            url = String.valueOf(hentOrdUrl.getText());
+            if(url.startsWith("www")){
+                url = "http://"+hentOrdUrl.getText();
+            }
+            AsyncTask task = new AsyncTask() {
+                @Override
+                protected Object doInBackground(Object... args0) {
+
+                    try {
+                        gl.hentOrd(url);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return "Success";
+                }
+            };
+            task.execute();
+            ord = gl.getMuligeOrd();
+            ordFraUrl.setAdapter(new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1, ord));
         }
     }
 }
